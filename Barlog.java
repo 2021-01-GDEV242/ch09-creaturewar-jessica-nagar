2e0b3999cd11f27cabcfcd20d158744190d4cd04
jessica-nagar <77754279+jessica-nagar@users.noreply.github.com>
2021-04-05 21:33:26 -0400

    Update core features of the Barlog class

--- Barlog.java
+++ Barlog.java
@@ -7,29 +7,40 @@
  * @author Jessica Nagar
  * @version 04.05.2021
  */
-public class Barlog
+public class Barlog extends Demon
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private static final int MAX_BARLOG_HP = 200;
+    private static final int MIN_BARLOG_HP = 80;
+    private static final int MAX_BARLOG_STR = 100;
+    private static final int MIN_BARLOG_STR = 50;
 
     /**
-     * Constructor for objects of class Barlog
+     * Constructor for objects of class Barlog -
+     * Note that the calling class does not need to know anything about the 
+     * requirements of barlog minimum and maximum values
+     * 
+     * The instantiating class asks for a Barlog and the barlog class is responsible for
+     * return a Barlog object with values in the appropriate range
+     * 
      */
     public Barlog()
     {
-        // initialise instance variables
-        x = 0;
+        // note how the class uses the static randomizer class to
+        // generate the values. This localizes the need to know 
+        // max and min values to this class only
+        // max-min is range of values
+        // range + min ensures that the values don't start at one.
+        super(
+            Randomizer.nextInt(MAX_BARLOG_HP-MIN_BARLOG_HP)+MIN_BARLOG_HP,    
+            Randomizer.nextInt(MAX_BARLOG_STR-MIN_BARLOG_STR)+MIN_BARLOG_STR
+            );
+          
     }
+    
+    
+    // attack() - not overridden because Humans generate basic damage
+    // takeDamage(int) - not overridden, because Humans take all damage assigned to them
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
