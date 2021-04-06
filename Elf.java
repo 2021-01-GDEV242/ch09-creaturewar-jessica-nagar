1df76b7870ac7a02f885c817f1678310a7d9194e
jessica-nagar <77754279+jessica-nagar@users.noreply.github.com>
2021-04-05 20:45:59 -0400

    Update Core features of the Elf class

--- Elf.java
+++ Elf.java
@@ -7,29 +7,44 @@
  * @author Jessica Nagar
  * @version 04.05.2021
  */
-public class Elf
+public class Elf extends Creature
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private static final int MAX_ELF_HP = 30;
+    private static final int MIN_ELF_HP = 10;
+    private static final int MAX_ELF_STR = 18;
+    private static final int MIN_ELF_STR = 5;
 
     /**
-     * Constructor for objects of class Elf
+     * Constructor for objects of class Elf -
+     * Note that the calling class does not need to know anything about the 
+     * requirements of human minimum and maximum values
+     * 
+     * The instantiating class asks for a Elf and the elf class is responsible for
+     * return a Elf object with values in the appropriate range
+     * 
      */
     public Elf()
     {
-        // initialise instance variables
-        x = 0;
+        // note how the class uses the static randomizer class to
+        // generate the values. This localizes the need to know 
+        // max and min values to this class only
+        // max-min is range of values
+        // range + min ensures that the values don't start at one.
+        super(
+            Randomizer.nextInt(MAX_ELF_HP-MIN_ELF_HP)+MIN_ELF_HP,    
+            Randomizer.nextInt(MAX_ELF_STR-MIN_ELF_STR)+MIN_ELF_STR
+        );
+          
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    
+    public void attack() 
     {
-        // put your code here
-        return x + y;
+        //damage method
     }
+    
+    // takeDamage(int) - not overridden, because Elf take all damage assigned to them
+
+    
 }
